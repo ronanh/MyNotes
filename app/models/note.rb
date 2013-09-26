70d171680c36239b6c8db57c4bd13794831efcad
ronanh <ronan.harmegnies@gmail.com>
2013-09-26 10:04:57 +0000

    ajout method search sur notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,6 @@
 class Note < ActiveRecord::Base
+	def self.search(t)
+		Note.where(title: t)
+	end
+
 end
--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "read note" do
+		note = notes(:one)
+		assert_equal note.title, "note1"
+		assert_equal note.content, "note1 content"
+	end
+
+	test "search note" do
+		notes = Note.search("note1")
+		assert_equal notes.length, 2
+	end
+
 end
