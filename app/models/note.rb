63ab120e7fc5ad38575fdc1da0bbefba8249733c
ronanh <ronan.harmegnies@gmail.com>
2013-09-26 15:57:08 +0000

    ajout de pagination avec kaminari

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,7 @@
 class Note < ActiveRecord::Base
 	validates_presence_of :title
-	
+	paginates_per 3
+
 	def self.search(keywords)
 		where 'lower(title) like :keywords OR lower(content) like :keywords', keywords: "%#{keywords.downcase}%"
 	end
